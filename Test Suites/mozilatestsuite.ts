29258d6a69870265bfaf4f73c97487cffef414b3
Sumit Tulsyan <sumittulsyan@Sumits-Macbook-Pro.local>
2019-08-24 22:29:31 +0530

    added

---
+++ Test Suites/mozilatestsuite.ts
@@ -0,0 +1,13 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>mozilatestsuite</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <testSuiteGuid>402496b8-253d-4776-8f39-32b24ed34596</testSuiteGuid>
+</TestSuiteEntity>
